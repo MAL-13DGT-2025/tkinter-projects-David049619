d39b7105c618130c0ff69d3dfcfeb3f04071daaf
David049619 <david.markahe@marcellin.school.nz>
2025-03-06 12:29:42 +1300

    created calculator GUI

---
+++ calculator.py
@@ -0,0 +1,17 @@
+import tkinter as tk #must Have
+from tkinter import ttk
+
+
+
+root = tk.Tk() #must Have
+
+calc = ttk.Entry(root)
+calc.grid(row = 0, column = 0, columnspan = 4)
+
+
+
+
+
+
+
+root.mainloop() #must Have
